0a52c0523aafcb2d99210000e856c1e917b6d845
Courtney Moran <courtney@DN51t4si.SUNet>
2021-05-11 15:17:28 -0700

    simulation setup

---
+++ build/CMakeFiles/simviz.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/courtney/Library/cs225a/SAI/apps/volleybot/simviz_volley.cpp" "/Users/courtney/Library/cs225a/SAI/apps/volleybot/build/CMakeFiles/simviz.dir/simviz_volley.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GL_SILENCE_DEPRECATION"
+  "MACOSX"
+  "USING_OTG"
+  "_MACOSX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "/Users/courtney/Library/cs225a/SAI/core/chai3d/src"
+  "/Users/courtney/Library/cs225a/SAI/core/chai3d/external/glew/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/Users/courtney/Library/cs225a/SAI/core/chai3d/external/DHD/include"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-simulation/src"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-model/src"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-model/rbdl/include"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-model/rbdl/build/include"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-graphics/src"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-common/src"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-urdfreader/src"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-urdfreader/src/urdf/urdfdom/urdf_parser/include"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-primitives/src"
+  "/Users/courtney/Library/cs225a/SAI/core/sai2-primitives/../external/ReflexxesTypeII/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
